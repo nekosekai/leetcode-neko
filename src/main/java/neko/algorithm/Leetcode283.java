7e7ef64410d096fd2e36549b6c4774b5a3e5baa0
Mr.neko <Mr.neko@protonmail.com>
2021-04-03 14:30:35 +0800

    update 283

--- src/main/java/neko/algorithm/Leetcode283.java
+++ src/main/java/neko/algorithm/Leetcode283.java
@@ -24,14 +24,27 @@ public class Leetcode283 {
     leetcode283.moveZeroes(arr);
   }
 
+  //  public void moveZeroes(int[] nums) {
+  //    for (int i = 0; i < nums.length - 1; i++) {
+  //      for (int j = 0; j < nums.length - 1 - i; j++) {
+  //        if (nums[j] == 0) {
+  //          int temp = nums[j];
+  //          nums[j] = nums[j + 1];
+  //          nums[j + 1] = temp;
+  //        }
+  //      }
+  //    }
+  //  }
+
   public void moveZeroes(int[] nums) {
-    for (int i = 0; i < nums.length - 1; i++) {
-      for (int j = 0; j < nums.length - 1 - i; j++) {
-        if (nums[j] == 0) {
-          int temp = nums[j];
-          nums[j] = nums[j + 1];
-          nums[j + 1] = temp;
+    int j = 0;
+    for (int i = 0; i <= nums.length; ++i) {
+      if (nums[i] != 0) {
+        nums[j] = nums[i];
+        if (i != j) {
+          nums[i] = 0;
         }
+        j++;
       }
     }
   }
