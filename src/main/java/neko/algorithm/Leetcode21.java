c76a4c7f38405ad1f1896f3ad488b7571a22eec4
Mr.neko <Mr.neko@protonmail.com>
2021-04-04 16:43:19 +0800

    update 21

--- src/main/java/neko/algorithm/Leetcode21.java
+++ src/main/java/neko/algorithm/Leetcode21.java
@@ -67,21 +67,4 @@ public class Leetcode21 {
   }
 }
 
-class ListNode {
-  int val;
 
-  ListNode next;
-
-  ListNode() {
-
-  }
-
-  ListNode(int val) {
-    this.val = val;
-  }
-
-  ListNode(int val, ListNode next) {
-    this.val = val;
-    this.next = next;
-  }
-}
